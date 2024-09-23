1a4a2e8dbc72131a4304a102a2a9b9f3ef9fe8a0
PedroBonelli <128149892+PedroBonelli@users.noreply.github.com>
2024-09-23 09:36:12 -0300

    Delete ms-alunos/ms-alunos directory

--- ms-alunos/ms-alunos/src/main/java/com/github/pedrobonelli/ms_alunos/model/Status.java
+++
@@ -1,8 +0,0 @@
-package com.github.pedrobonelli.ms_alunos.model;
-
-public enum Status {
-    MATRICULADO,
-    TRANCADO,
-    FORMADO,
-    CANCELADO
-}
--- ms-alunos/ms-alunos/src/main/java/com/github/pedrobonelli/ms_alunos/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.pedrobonelli.ms_alunos.repository;
-
-import com.github.pedrobonelli.ms_alunos.model.AlunoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
-}
